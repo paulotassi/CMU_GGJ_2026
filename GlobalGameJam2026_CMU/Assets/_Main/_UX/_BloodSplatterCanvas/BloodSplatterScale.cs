560484e09cc3a74788911de9dc2e9b648d5f3637
Austin Szema <aszema16@gmail.com>
2026-01-31 18:17:26 -0500

    script for blood splatter to scale with player health

---
+++ GlobalGameJam2026_CMU/Assets/_Main/_UX/_BloodSplatterCanvas/BloodSplatterScale.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class BloodSplatterScale : MonoBehaviour
+{
+    [SerializeField] private RectTransform bloodSplatterImage;
+
+    private float maxScale = 4.5f;
+    
+    private float minScale = 2f;
+    
+    [SerializeField] private PlayerController player;
+    
+    private void Update()
+    {
+        ScaleBloodSplatterToPlayerHealth();
+    }
+
+    
+    /// <summary>
+    /// Scales the blood splatter image scale (2-4.5) relative to player health (0-100)
+    /// </summary>
+    private void ScaleBloodSplatterToPlayerHealth()
+    {
+        float lerp = Mathf.Lerp(minScale, maxScale, player.playerHealth / player.startingPlayerHealth);
+        bloodSplatterImage.localScale = new Vector3(lerp, lerp, lerp);
+        
+    }
+}
